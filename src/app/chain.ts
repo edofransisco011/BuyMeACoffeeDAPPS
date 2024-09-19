7b6b243b9f89466edf8f292c503160b777b70c48
edofransisco011 <edofransisco011@gmail.com>
2024-09-19 10:22:35 +0700

    first commit

---
+++ src/app/chain.ts
@@ -0,0 +1,4 @@
+import { defineChain } from "thirdweb";
+import { sepolia } from "thirdweb/chains";
+
+export const chain = defineChain(sepolia);
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      height: '100vh',
+      marginTop: '2rem',
+      }} >
+        <h1>Buy Me A Coffee</h1>
+    </div>
+  );
+}
